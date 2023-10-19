c220c45e8e35da7e59b1ba4f9a0d8af5f5b35b3f
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2023-10-19 23:20:29 +0700

    feat: initialize next

---
+++ src/app/user/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function Login() {
+    return (
+        <>
+            <h1>
+                Login page
+            </h1>
+        </>
+    )
+}
---
+++ src/app/user/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export default function User() {
+    return (
+        <>
+            <h1>
+                User page
+            </h1>
+        </>
+    )
+}
